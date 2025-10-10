1b6039bd45fc330e6b4178142df64ac15f45a252
Anton <anton.chivkunov@gmail.com>
2025-10-10 16:40:29 +0200

    Docker-compose: auto-rebuild app if content of Docker file changed
    This doesn't happen by default, we have to force docker do this.

--- tests/unit/test_runo.py
+++ tests/unit/test_runo.py
@@ -1233,7 +1233,7 @@ class TestDockerComposeServiceCommands(BaseCommandsTest):
                 "docker",
                 "compose",
                 *expected_docker_compose_options,
-                "run",
+                "run --build",
             ]
             + _expected_docker_run_options(docker_run_options_str)
             + [container_config["docker_compose_service"]]
